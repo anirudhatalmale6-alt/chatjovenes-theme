b76a0a0acffae448162084810a059c0dec4cad9c
Anirudha Talmale <anirudhatalmale6-alt@users.noreply.github.com>
2026-06-20 14:21:37 +0000

    v1.9.7: Footer 4 columns like ChatZona - Top/Ultimos canales, Corporativo, Legal with icons

--- footer.php
+++ footer.php
@@ -13,34 +13,12 @@
 <footer class="site-footer">
     <div class="container">
         <div class="footer-columns">
-            <div class="footer-col">
-                <h4><?php bloginfo('name'); ?></h4>
-                <p><?php bloginfo('description'); ?></p>
-                <div class="social-icons">
-                    <?php
-                    $socials = array(
-                        'facebook'  => 'F',
-                        'twitter'   => 'X',
-                        'instagram' => 'IG',
-                        'youtube'   => 'YT',
-                    );
-                    foreach ($socials as $key => $icon) :
-                        $url = get_theme_mod("social_$key", '');
-                        if ($url) :
-                    ?>
-                        <a href="<?php echo esc_url($url); ?>" target="_blank" rel="noopener" title="<?php echo esc_attr(ucfirst($key)); ?>"><?php echo $icon; ?></a>
-                    <?php
-                        endif;
-                    endforeach;
-                    ?>
-                </div>
-            </div>
-            <div class="footer-col">
-                <?php if (is_active_sidebar('footer-2')) : ?>
-                    <?php dynamic_sidebar('footer-2'); ?>
+            <div class="footer-col footer-col-channels">
+                <?php if (is_active_sidebar('footer-1')) : ?>
+                    <?php dynamic_sidebar('footer-1'); ?>
                 <?php else : ?>
                     <h4>Top Canales</h4>
-                    <ul>
+                    <ul class="footer-icon-list">
                         <?php
                         $top_rooms = new WP_Query(array(
                             'post_type'      => 'chat_room',
@@ -58,12 +36,12 @@
                     </ul>
                 <?php endif; ?>
             </div>
-            <div class="footer-col">
-                <?php if (is_active_sidebar('footer-3')) : ?>
-                    <?php dynamic_sidebar('footer-3'); ?>
+            <div class="footer-col footer-col-channels">
+                <?php if (is_active_sidebar('footer-2')) : ?>
+                    <?php dynamic_sidebar('footer-2'); ?>
                 <?php else : ?>
                     <h4>Ultimos Canales</h4>
-                    <ul>
+                    <ul class="footer-icon-list">
                         <?php
                         $latest_rooms = new WP_Query(array(
                             'post_type'      => 'chat_room',
@@ -81,16 +59,29 @@
                     </ul>
                 <?php endif; ?>
             </div>
+            <div class="footer-col footer-col-corp">
+                <?php if (is_active_sidebar('footer-3')) : ?>
+                    <?php dynamic_sidebar('footer-3'); ?>
+                <?php else : ?>
+                    <h4>Corporativo</h4>
+                    <ul class="footer-check-list">
+                        <li><a href="#">Historia</a></li>
+                        <li><a href="#">Equipo</a></li>
+                        <li><a href="#">Registro</a></li>
+                        <li><a href="#">Crea tu chat</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                <?php endif; ?>
+            </div>
             <div class="footer-col footer-col-legal">
                 <?php if (is_active_sidebar('footer-4')) : ?>
                     <?php dynamic_sidebar('footer-4'); ?>
                 <?php else : ?>
                     <h4>Legal</h4>
-                    <ul>
-                        <li><a href="<?php echo esc_url(get_privacy_policy_url()); ?>">Politica de Privacidad</a></li>
+                    <ul class="footer-check-list">
                         <li><a href="#">Aviso Legal</a></li>
+                        <li><a href="<?php echo esc_url(get_privacy_policy_url()); ?>">Politica de Privacidad</a></li>
                         <li><a href="#">Politica de Cookies</a></li>
-                        <li><a href="#">Contacto</a></li>
                     </ul>
                 <?php endif; ?>
             </div>
